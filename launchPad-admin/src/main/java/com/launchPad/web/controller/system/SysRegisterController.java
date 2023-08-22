fb8921bab1844d47ad7b1fcde836ef31e7c407ea
always <1169791070@qq.com>
2023-08-22 15:41:22 +0800

    launchpad

---
+++ launchPad-admin/src/main/java/com/launchPad/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.launchPad.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.launchPad.common.core.controller.BaseController;
+import com.launchPad.common.core.domain.AjaxResult;
+import com.launchPad.common.core.domain.model.RegisterBody;
+import com.launchPad.common.utils.StringUtils;
+import com.launchPad.framework.web.service.SysRegisterService;
+import com.launchPad.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
---
+++ launchPad-common/src/main/java/com/launchPad/common/core/domain/model/RegisterBody.java
@@ -0,0 +1,11 @@
+package com.launchPad.common.core.domain.model;
+
+/**
+ * 用户注册对象
+ * 
+ * @author ruoyi
+ */
+public class RegisterBody extends LoginBody
+{
+
+}
